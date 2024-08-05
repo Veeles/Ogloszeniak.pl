902925aac314e140cbb1ec62ed8b56f1a8f0fc1d
Veeles <kacper.ros2@interia.pl>
2024-08-05 13:15:41 +0200

    Complete creating base, footer, header.html

--- ogloszeniak/ogloszeniak/settings.py
+++ ogloszeniak/ogloszeniak/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'listings',
+    'notificactions',
+    'payments',
+    'search',
+    'users',
+    'bootstrap5',
+    'crispy_forms',
+    "crispy_bootstrap5"
 ]
 
 MIDDLEWARE = [
@@ -116,8 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
---
+++ ogloszeniak/ogloszeniak/views.py
@@ -0,0 +1,6 @@
+from django.template import loader
+from django.http import HttpResponse
+
+def home_view(request):
+    template =  loader.get_template('base.html')
+    return HttpResponse(template.render(request))
