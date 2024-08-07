8f1dfcd05b9e94137cfdc3af1ff43f32d8978775
Veeles <kacper.ros2@interia.pl>
2024-08-07 16:55:43 +0200

    Create product and category models
    Add categories
    
    Start stylizating index.html

--- ogloszeniak/listings/models.py
+++ ogloszeniak/listings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+  
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.ImageField()
+    crated_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='listings')
+    
+    def __str__(self):
+        return self.name
--- ogloszeniak/ogloszeniak/views.py
+++ ogloszeniak/ogloszeniak/views.py
@@ -2,5 +2,5 @@ from django.template import loader
 from django.http import HttpResponse
 
 def home_view(request):
-    template =  loader.get_template('base.html')
+    template =  loader.get_template('index.html')
     return HttpResponse(template.render())
