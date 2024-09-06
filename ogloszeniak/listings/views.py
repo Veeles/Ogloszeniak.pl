84347eec7ab5e67db938ca6b176baf7e3b274551
Veeles <kacper.ros2@interia.pl>
2024-09-06 14:13:48 +0200

    Complete creating template product.html and connect it with listing models, .views, .urls

--- ogloszeniak/listings/views.py
+++ ogloszeniak/listings/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from django.http import HttpResponse
+from django.template import loader
 
+def product_details(request, id):
+    product = get_object_or_404(Product, id=id)
+    template =  loader.get_template('product.html')
+    print(product.photo.path)  # Pełna ścieżka do pliku
+    print(product.photo.url)  # URL obrazu
+    context = {
+        'product':product
+    }
+    return HttpResponse(template.render(context,request))
 # Create your views here.
