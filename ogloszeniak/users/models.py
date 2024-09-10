2e7cce8d107d589911be34d8dbd00de31a74976f
Veeles <kacper.ros2@interia.pl>
2024-09-10 14:30:04 +0200

    Complete creating register.html and connect it with other piece of code

--- ogloszeniak/users/models.py
+++ ogloszeniak/users/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+# from django import forms
+# from django.contrib.auth import login, authenticate
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+# # Create your models here.
 
-# Create your models here.
+# class RegisterForm(UserCreationForm):
+#     email = forms.EmailField()
+
+#     class Meta:
+#         model = User
+#         filds = ["username", 'email', 'password1', 'password2']
--- ogloszeniak/users/views.py
+++ ogloszeniak/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.contrib.auth import login, authenticate
+# from django.contrib.auth.forms import UserCreationForm
+from django.template import loader
+from django.http import HttpResponse
+from .forms import RegisterForm
 
+def login_view(response):
+    if response.method == 'POST':
+        pass
+def register_view(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = RegisterForm()
+    template = loader.get_template('register.html')
+    context = {
+        'form':form
+    }
+    return HttpResponse(template.render(context,response))
 # Create your views here.
